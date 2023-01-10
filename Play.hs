4c9bd162897458b2db78e2378b94a2d3dbde3636
mgarbula <mgarbula@student.agh.edu.pl>
2023-01-10 14:06:36 +0100

    new type added

--- Play.hs
+++ Play.hs
@@ -1,37 +1,40 @@
 module Play where
 import Printing
 
-type Move = (String, String)
+type Move = (Position, Position)
 type Color = Char
+type Position = (Char, Integer)
 
 fromTo :: String -> Move
-fromTo move = (takeWhile (/= '-') move, tail (dropWhile (/= '-') move))
+fromTo move = ((head firstPosition, read (tail firstPosition) :: Integer), (head secondPosition, read (tail secondPosition) :: Integer))
+    where firstPosition = takeWhile (/= '-') move
+          secondPosition = tail (dropWhile (/= '-') move)
 
 takeFirstElements board move = takeWhile (\b -> first (head b) /= move) board
 takeRestElements board move = dropWhile (\b -> first (head b) /= move) board
 
-changePosition :: [Field] -> String -> Color -> [Field]
+changePosition :: [Field] -> Position -> Color -> [Field]
 changePosition [] _ _ = []
 changePosition (x:xs) s color
-    | second x == head s = (first x, second x, color) : changePosition xs s color
+    | second x == fst s = (first x, second x, color) : changePosition xs s color
     | otherwise = x : changePosition xs s color
 
 changePositionBoard :: [[Field]] -> String -> Color -> [[Field]]
 changePositionBoard board move color = takeFirstElements board number ++ [changePosition (head rest) (snd myMove) color] ++ tail rest
     where myMove = fromTo move
-          number = read (tail (snd myMove))
+          number = snd (snd myMove)
           rest = takeRestElements board number
 
-clearPosition :: [Field] -> String -> [Field]
+clearPosition :: [Field] -> Position -> [Field]
 clearPosition [] _ = []
 clearPosition (x:xs) s 
-    | second x == head s = (first x, second x, ' ') : clearPosition xs s
+    | second x == fst s = (first x, second x, ' ') : clearPosition xs s
     | otherwise = x : clearPosition xs s
 
 clearPositionBoard :: [[Field]] -> String -> [[Field]]
 clearPositionBoard board move = takeFirstElements board number ++ [clearPosition (head rest) (fst myMove)] ++ tail rest
     where myMove = fromTo move
-          number = read (tail (fst myMove))
+          number = snd (fst myMove)
           rest = takeRestElements board number
 
 makeMove :: [[Field]] -> String -> Color -> [[Field]]
