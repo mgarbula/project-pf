b0a1ea092129b080513d62f06cfd49ca09ed37b5
mgarbula <mgarbula@student.agh.edu.pl>
2023-01-18 00:09:36 +0100

    jump forward

--- Oponent.hs
+++ Oponent.hs
@@ -5,15 +5,41 @@ import Printing
 target = map (\l -> (7, l)) topLetters ++ map (\l -> (8, l)) topLetters
 
 oponentFields :: [[Field]] -> Color -> [Field]
-oponentFields board color = playerFields board color
+oponentFields board color = filter (\f -> first f /= 7 || first f /= 8) (playerFields board color)
 
 isNextEmpty :: [[Field]] -> Field -> Bool
-isNextEmpty board field = third (head (filter (\f -> second f == second field) row)) == ' '
+isNextEmpty board field  = third (head (filter (\f -> second f == second field) row)) == ' '
     where row = head (dropWhile (\r -> first (head r) /= (first field) + 1) board)
 
 moveForwardPossible :: [[Field]] -> [Field] -> (Bool, Position)
-moveForwardPossible board [] = (False, ('Z', 9))
+moveForwardPossible _ [] = (False, ('Z', 9))
 moveForwardPossible board (x:xs) 
     | isNextEmpty board x = (True, (second x, first x))
     | otherwise = moveForwardPossible board xs
-    
+
+isFieldEmpty :: [[Field]] -> Integer -> Integer -> Bool
+isFieldEmpty board column row
+    | row <= 0 || column <= 0 = False
+    | elem column (map (\(a, b) -> b) valueOfLetters) == False = False -- sprawdzam czy kolumna do ktorej chce przejsc istnieje
+    | third (head (dropWhile (\c -> second c /= columnLetter) (head (dropWhile (\r -> first (head r) /= row) board)))) /= ' ' = False
+    | otherwise = True
+    where columnLetter = fst (head (dropWhile (\(_, val) -> val /= column) valueOfLetters))
+
+getColumn columnValue = head (dropWhile (\(_, value) -> value /= columnValue + 1) valueOfLetters)
+isSomeJumpPossible :: [[Field]] -> Field -> (Bool, Position, Position)
+isSomeJumpPossible board field 
+    | isFieldEmpty board (columnValue + 1) (row + 2) = (True, (second field, row), (fst (getColumn columnValue), row + 2))
+    | isFieldEmpty board (columnValue + 1) (row - 2) = (True, (second field, row), (fst (getColumn columnValue), row - 2))
+    | isFieldEmpty board (columnValue + 2) (row + 1) = (True, (second field, row), (fst (getColumn (columnValue + 1)), row + 1))
+    | isFieldEmpty board (columnValue - 2) (row + 1) = (True, (second field, row), (fst (getColumn (columnValue - 3)), row + 1)) -- getColumn dodaje 1, więc musze odjąc 3
+    | otherwise = (False, ('Z', 9), ('Z', 9))
+    where columnValue = snd (head (dropWhile (\(letter, _) -> letter /= second field) valueOfLetters))
+          row = first field
+
+valueOfColumn field = snd (head (dropWhile (\(letter, _) -> letter /= second field) valueOfLetters))
+-- jumpPossible :: [[Field]] -> [Field] -> (Bool, Position, Position)
+-- jumpPossible _ [] = (False, ('Z', 9), ('Z', 9))
+-- jumpPossible board (x:xs)
+--     let someJump = isSomeJumpPossible board x
+--     | first someJump = someJump
+--     | otherwise = jumpPossible board xs
