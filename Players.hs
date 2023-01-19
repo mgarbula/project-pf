a5454ae1a0479a82dbcb817aa6a602c8a7f68d0c
mgarbula <mgarbula@student.agh.edu.pl>
2023-01-19 16:07:47 +0100

    function to update board

--- Players.hs
+++ Players.hs
@@ -22,6 +22,18 @@ playersMove board color = do
             putStrLn "Niedozwolony ruch"
             playersMove board color
 
+updateBoard :: [[Field]] -> String -> Char -> String -> IO()
+updateBoard board move myColor color = do
+    putStrLn move
+    let newBoard = makeMove board move myColor
+    let myFields = changePlayerField (playerFields board myColor) (fromTo move) myColor
+    putStrLn (printAll newBoard)
+    if possibleWin myFields 7 8
+        then do
+            putStrLn "Koniec gry! Wygral komputer!"
+        else do
+            playersMove newBoard color
+
 oponent :: [[Field]] -> String -> IO()
 oponent board color = do
     putStrLn "Ruch przeciwnika"
@@ -32,18 +44,18 @@ oponent board color = do
         then do
             let position = snd possibleMove
             let move = fst position : (show (snd position) ++ ('-' : (fst position) : show (snd position + 1)))
-            putStrLn move
-            let newBoard = makeMove board move myColor
-            let myFields = changePlayerField (playerFields board myColor) (fromTo move) myColor
-            putStrLn (printAll newBoard)
-            if possibleWin myFields 7 8
-                then do
-                    putStrLn "Koniec gry! Wygrales!"
-                else do
-                    playersMove newBoard color
+            updateBoard board move myColor color            
+    else do
+        let possibleMove = jumpPossible board myFields
+        if first possibleMove
+            then do
+                let from = second possibleMove
+                let to = third possibleMove
+                let move = fst from : (show (snd from) ++ ('-' : (fst to) : show (snd to)))
+                updateBoard board move myColor color
         else do
-            putStr ""
-
+            putStrLn "Brak ruchu"
+            playersMove board color
 
 go :: [[Field]] -> String -> IO ()
 go board color = do
