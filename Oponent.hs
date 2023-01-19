01837dd08cacc6028d71b2a9e5beb6306c2401c6
mgarbula <mgarbula@student.agh.edu.pl>
2023-01-19 18:08:34 +0100

    jump back

--- Oponent.hs
+++ Oponent.hs
@@ -22,6 +22,7 @@ moveForwardPossible board (x:xs)
 isFieldEmpty :: [[Field]] -> Integer -> Integer -> Bool
 isFieldEmpty board column row
     | row <= 0 || column <= 0 = False
+    | row > 8 || column > 8 = False
     | elem column (map (\(a, b) -> b) valueOfLetters) == False = False -- sprawdzam czy kolumna do ktorej chce przejsc istnieje
     | third (head (dropWhile (\c -> second c /= columnLetter) (head (dropWhile (\r -> first (head r) /= row) board)))) /= ' ' = False
     | otherwise = True
@@ -44,3 +45,20 @@ jumpForwardPossible board (x:xs)
     | first someJump = someJump
     | otherwise = jumpForwardPossible board xs
     where someJump = isSomeJumpPossible board x
+
+isSomeJumpBackPossible :: [[Field]] -> Field -> (Bool, Position, Position)
+isSomeJumpBackPossible board field
+    | isFieldEmpty board (columnValue + 1) (row - 2) = (True, (second field, row), (fst (getColumn columnValue), row - 2))
+    | isFieldEmpty board (columnValue - 1) (row - 2) = (True, (second field, row), (fst (getColumn (columnValue - 2)), row - 2))
+    | isFieldEmpty board (columnValue + 2) (row - 1) = (True, (second field, row), (fst (getColumn (columnValue + 1)), row - 1))
+    | isFieldEmpty board (columnValue - 2) (row + 1) = (True, (second field, row), (fst (getColumn (columnValue - 3)), row - 1))
+    | otherwise = (False, ('Z', 9), ('Z', 9))
+    where columnValue = snd (head (dropWhile (\(letter, _) -> letter /= second field) valueOfLetters))
+          row = first field
+
+jumpBackPossible :: [[Field]] -> [Field] -> (Bool, Position, Position)
+jumpBackPossible _ [] = (False, ('Z', 9), ('Z', 9))
+jumpBackPossible board (x:xs)
+    | first someJump = someJump
+    | otherwise = jumpBackPossible board xs
+    where someJump = isSomeJumpBackPossible board x
