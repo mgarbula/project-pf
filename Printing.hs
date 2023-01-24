7e8bfa544cc724a720a2ed10a8722d814f71a8ad
mgarbula <mgarbula@student.agh.edu.pl>
2023-01-24 11:31:24 +0100

    add letters at bottom and numbers on right

--- Printing.hs
+++ Printing.hs
@@ -41,21 +41,23 @@ printLetter :: Char -> [Char]
 printLetter x = "|  " ++ [x] ++ "  "
 
 printMultipleEquals :: Int -> [Char]
-printMultipleEquals 0 = endLine 
+printMultipleEquals 0 = "||=\n" 
 printMultipleEquals x = printEquals ++ printMultipleEquals (x - 1)
 
-printTop :: [Char] -> [Char]    
-printTop [] = endLine ++ "=|" ++ printMultipleEquals lengthOfBoard
-printTop (x:xs) 
-    | length (x:xs) == 8 = " |" ++ printLetter x ++ printTop xs
-    | otherwise = printLetter x ++ printTop xs
+printSeparator = "=|" ++ printMultipleEquals lengthOfBoard 
+
+printLetters :: [Char] -> [Char]    
+printLetters [] = endLine
+printLetters (x:xs) 
+    | length (x:xs) == 8 = " |" ++ printLetter x ++ printLetters xs
+    | otherwise = printLetter x ++ printLetters xs
 
 printEmptyRow :: Int -> [Char]
 printEmptyRow 0 = endLine
 printEmptyRow x = printSpaceForJumper ++ printEmptyRow (x - 1)
 
 printLineRow :: Int -> [Char]
-printLineRow 0 = endLine
+printLineRow 0 = "||-\n"
 printLineRow x   
     | x == lengthOfBoard = "-|" ++ printLine ++ printLineRow (x - 1)
     | otherwise = printLine ++ printLineRow (x - 1)
@@ -63,16 +65,18 @@ printLineRow x
 printSymbol :: Field -> [Char]
 printSymbol x = "|  " ++ [third x] ++ "  "
 
-printRow :: [Field] -> [Char]
-printRow [] = endLine
-printRow (x:xs) 
-    | length (x:xs) == lengthOfBoard = printNumber ++ printSymbol x ++ printRow xs
-    | otherwise = printSymbol x ++ printRow xs
-    where printNumber = show (first x) ++ "|" 
+printRow :: [Field] -> Integer -> [Char]
+printRow [] x = "||" ++ show x ++ "\n"
+printRow (x:xs) num
+    | length (x:xs) == lengthOfBoard = printNumber ++ "|" ++ printSymbol x ++ printRow xs (first x)
+    | otherwise = printSymbol x ++ printRow xs (first x)
+    where printNumber = show (first x)  
 
 printRest :: [[Field]] -> [Char]
-printRest [] = "=|" ++ printMultipleEquals lengthOfBoard
-printRest (x:xs) = printRow x ++ (printLineRow lengthOfBoard) ++ printRest xs
+printRest [] = ""
+printRest (x:xs)
+    | length (x:xs) == 1 = printRow x 0 ++ "=|" ++ printMultipleEquals lengthOfBoard
+    | otherwise = printRow x 0 ++ (printLineRow lengthOfBoard) ++ printRest xs
 
 printAll :: [[Field]] -> [Char] 
-printAll board = printTop topLetters ++ printRest board
+printAll board = printLetters topLetters ++ printSeparator ++ printRest board ++ printLetters topLetters
